2d2881dc7f359c87a21e964737ca9873a8f1bddd
Connor Carpenter <hln-ccar@sheetz.com>
2021-11-29 00:12:43 -0500

    Removed target sources from git repo

--- target/classes/db.migration/postgresql/V2021.11.26.01.5__create_achievement.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS achievement
-(
-    achievement_id SERIAL PRIMARY KEY,
-    game_id        INT NOT NULL,
-    title          varchar(128),
-    description    varchar(1024),
-    CONSTRAINT FK_game_id_achievement FOREIGN KEY (game_id) REFERENCES game (game_id)
-);
